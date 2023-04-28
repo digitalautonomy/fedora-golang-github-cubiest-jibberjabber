dd33fbcc21f3241e318e46422d5e2896c4f72c8b
Fausto M. Lagos S <fausto@autonomia.digital>
2023-04-28 16:28:50 -0500

    Changed the use of specific directories for each fedora version

--- build_all.sh
+++ build_all.sh
@@ -6,10 +6,9 @@ build_for_release() {
     local release=$1;
 
     echo "Building for $release ..."
-    mkdir -p $release
     fedpkg --release $release local
-    ssh -p 4243 fedora-repo@fedora.autonomia.digital "mkdir -p packages/upload/$release"
-    scp -r -P4243 $release/* fedora-repo@fedora.autonomia.digital:packages/upload/$release
+    scp -r -P4243 *.rpm fedora-repo@fedora.autonomia.digital:packages/upload/$release
+    rm -rf *.rpm
     echo "Done build for $release!"
 }
 
